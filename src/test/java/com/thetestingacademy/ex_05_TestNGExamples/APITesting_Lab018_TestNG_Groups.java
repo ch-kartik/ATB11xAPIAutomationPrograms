948bfe0663eacf569cf147ea2444169ecf994e28
ch-kartik <kartikch619@gmail.com>
2025-08-26 17:13:48 +0530

    pushed on 26 August 2025

---
+++ src/test/java/com/thetestingacademy/ex_05_TestNGExamples/APITesting_Lab018_TestNG_Groups.java
@@ -0,0 +1,30 @@
+package com.thetestingacademy.ex_05_TestNGExamples;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class APITesting_Lab018_TestNG_Groups {
+
+    // sanity -> 1
+    // reg -> 3
+    // p1 -> 2
+
+    @Test(groups = {"sanity", "reg"}, priority = 1)
+    public void test_sanityRun(){
+        System.out.println("Sanity");
+        System.out.println("QA");
+        Assert.assertTrue(true);
+    }
+
+    @Test(groups = {"reg"}, priority = 2)
+    public void test_regRun(){
+        System.out.println("Reg");
+        Assert.assertTrue(false);
+    }
+
+    @Test(groups = {"p1", "reg"}, priority = 3)
+    public void test_smokeRun(){
+        System.out.println("Smoke");
+        Assert.assertTrue(false);
+    }
+}
