cc8c0c7c766c05c6b2c1f7f88ac5df8ec00b8437
ch-kartik <kartikch619@gmail.com>
2025-08-27 23:15:32 +0530

    pushed on 27 August 2025

---
+++ src/test/java/com/thetestingacademy/ex_05_TestNGExamples/APITesting_Lab019_TestNG_DependsOnMethod.java
@@ -0,0 +1,25 @@
+package com.thetestingacademy.ex_05_TestNGExamples;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class APITesting_Lab019_TestNG_DependsOnMethod {
+
+    @Test
+    public void serverStartedOK(){
+        System.out.println("I will run first");
+        Assert.assertTrue(true);
+    }
+
+    @Test(dependsOnMethods = "serverStartedOK")
+    public void test1(){
+        System.out.println("method1");
+        Assert.assertTrue(true);
+    }
+
+    @Test(dependsOnMethods = "serverStartedOK")
+    public void test2(){
+        System.out.println("method2");
+        Assert.assertTrue(true);
+    }
+}
